535ec3c50be20c33ff084948dabc885310c652f4
michelle-b7 <bai7michelle@gmail.com>
2026-01-09 17:35:12 +0000

    trying bruh

---
+++ basic_prompt.js
@@ -0,0 +1,13 @@
+/**
+ * Converts a string to camelCase.
+ * Handles spaces, hyphens, and underscores.
+ */
+function toCamelCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+}
+
+// Verification:
+console.log(toCamelCase("get_user_info")); // "getUserInfo"
+console.log(toCamelCase("CSS-style-property")); // "cssStyleProperty"
