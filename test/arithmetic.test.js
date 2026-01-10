2340b69ec40f517477c9d395f086b790b4d04084
michelle-b7 <bai7michelle@gmail.com>
2026-01-10 00:46:12 +0000

    trying to commit tests

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -205,4 +205,42 @@ describe('Arithmetic', function () {
                 });
         });
     });
+
+    describe('Power', function () {
+        it('raises a positive integer to a positive power', function (done) {
+            request.get('/arithmetic?operation=pow&operand1=2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 8 });
+                    done();
+                });
+        });
+
+        it('raises a number to the power of zero', function (done) {
+            request.get('/arithmetic?operation=pow&operand1=42&operand2=0')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 1 });
+                    done();
+                });
+        });
+
+        it('raises a negative base to an even power', function (done) {
+            request.get('/arithmetic?operation=pow&operand1=-2&operand2=2')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 4 });
+                    done();
+                });
+        });
+
+        it('handles fractional exponents (square root)', function (done) {
+            request.get('/arithmetic?operation=pow&operand1=16&operand2=0.5')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 4 });
+                    done();
+                });
+        });
+    });
 });
